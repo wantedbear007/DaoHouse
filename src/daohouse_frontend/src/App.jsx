46ba0a0fb8a2f4475471af8b1e23c56550317436
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-04-28 09:10:43 +0530

    FeedPage

--- src/daohouse_frontend/src/App.jsx
+++ src/daohouse_frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
 // import Login from './Components/Auth/Login';
-import Navbar from './Components/layouts/Navbar';
-import Error404 from './Components/utils/Error404';
-import Dashboard from './Components/Home/Dashboard';
-import "./App.css"
-import Footer from './Components/layouts/Footer';
+import Navbar from "./Components/layouts/Navbar";
+import Error404 from "./Components/utils/Error404";
+import Dashboard from "./Components/Home/Dashboard";
+import Footer from "./Components/layouts/Footer";
+import FeedPage from "./Components/FeedPage/FeedPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/*" element={<Error404 />} />
         <Route path="/" element={<Dashboard />} />
+        <Route path="/feed" element={<FeedPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 };
-
 
 export default App;
