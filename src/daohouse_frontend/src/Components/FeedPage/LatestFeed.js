6ae5f28a9d89d708ba39ff231233ff909bc88f59
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-04-30 12:11:06 +0530

    Feeds Page done

---
+++ src/daohouse_frontend/src/Components/FeedPage/LatestFeed.js
@@ -0,0 +1,43 @@
+import img from "../../../assets/author.png";
+import img1 from "../../../assets/postMedia1.png";
+import img2 from "../../../assets/postMedia2.png";
+import img3 from "../../../assets/postMedia3.png";
+
+export default [
+  {
+    userImg: img,
+    userName: "Kai Parker",
+    content:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    time: 600,
+    link: "#",
+    postMedia: img2,
+  },
+  {
+    userImg: img,
+    userName: "Kai Parker",
+    content:
+      "Lorem ipsum dolor sits nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    time: 600,
+    link: "#",
+    postMedia: img3,
+  },
+  {
+    userImg: img,
+    userName: "Kai Parker",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    time: 600,
+    link: "#",
+    postMedia: img1,
+  },
+  {
+    userImg: img,
+    userName: "Kai Parker",
+    content:
+      "cing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    time: 600,
+    link: "#",
+    postMedia: img1,
+  },
+];
--- src/daohouse_frontend/src/Components/FeedPage/PostCard.jsx
+++ src/daohouse_frontend/src/Components/FeedPage/PostCard.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
 
-const PostCard = () => {
-    return (
-        <div>PostCard</div>
-    )
-}
+import { IoLink } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa6";
+import { PiTelegramLogoBold } from "react-icons/pi";
+import { MdOutlineInsertComment } from "react-icons/md";
 
-export default PostCard
+const PostCard = ({ post }) => {
+  const className = "postCard";
+
+  return (
+    <div
+      className={
+        className +
+        " " +
+        "w-full h-[300px] flex flex-row items-start gap-12 bg-white p-8 rounded-lg justify-between"
+      }
+    >
+      <section
+        className={className + "__rightSide h-full w-3/5 flex flex-col justify-between"}
+      >
+        <div className="flex flex-row items-center justify-between">
+          <section
+            className={
+              className + "__userData flex flex-row items-center gap-2"
+            }
+          >
+            <img
+              src={post.userImg}
+              alt="userImage"
+              className="rounded-[50%] w-10 h-10"
+            />
+            <p>{post.userName}</p>
+          </section>
+
+          <section className={className + "__time"}>
+            {post.time / 60} min ago
+          </section>
+        </div>
+
+        <div className="h-[70px]">
+          <p className="h-full overflow-hidden line-clamp-3">{post.content}</p>
+        </div>
+
+
+        <div className={className + "__buttons flex flex-row items-center gap-4 mt-8"}>
+          <button className="flex flex-row gap-2 items-center bg-[#0E3746] text-white py-3 px-8 rounded-[2rem]">
+            <FaRegHeart />
+            Like
+          </button>
+          <button className="flex flex-row gap-2 items-center bg-[#0E3746] text-white py-3 px-8 rounded-[2rem]">
+            <MdOutlineInsertComment />
+            Comment
+          </button>
+          <button className="flex flex-row gap-2 items-center bg-[#0E3746] text-white py-3 px-8 rounded-[2rem]">
+            <PiTelegramLogoBold />
+            Share
+          </button>
+
+          <button className="m-4">
+            <IoLink className="text-2xl" />
+          </button>
+        </div>
+      </section>
+
+      <section className={className + "__leftSide w-2/5 h-full"}>
+        {post.postMedia && (
+          <img
+            src={post.postMedia}
+            alt="POST Media"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default PostCard;
