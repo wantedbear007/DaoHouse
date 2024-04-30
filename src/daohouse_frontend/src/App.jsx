9c77e28bdd0baf2ae8c405b259c1aad3cfa9c460
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-04-30 11:15:59 +0530

    Routes

--- src/daohouse_frontend/src/App.jsx
+++ src/daohouse_frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from "react";
 import "./App.css";
 // import Login from './Components/Auth/Login';
@@ -8,20 +7,8 @@ import Dashboard from "./Components/Home/Dashboard";
 import Footer from "./Components/layouts/Footer";
 import FeedPage from "./Components/FeedPage/FeedPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
-import React from 'react';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-// import Login from './Components/Auth/Login';
-import Navbar from './Components/layouts/Navbar';
-import Error404 from './Components/utils/Error404';
-import Dashboard from './pages/Home/Dashboard';
-import "./App.css"
-import Footer from './Components/layouts/Footer';
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
+
 import Dao from './pages/dao/Dao';
->>>>>>> main
 
 const App = () => {
 
@@ -31,25 +18,14 @@ const App = () => {
       <Routes>
         <Route path="/*" element={<Error404 />} />
         <Route path="/" element={<Dashboard />} />
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 
         <Route path="/feed" element={<FeedPage />} />
       </Routes>
+      
+      <Route path="/dao" element={<Dao />} />
       <Footer />
-=======
-=======
-        <Route path="/dao" element={<Dao />} />
->>>>>>> main
-      </Routes>
-      <Footer/>
->>>>>>> Stashed changes
     </Router>
   );
 };
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 export default App;
