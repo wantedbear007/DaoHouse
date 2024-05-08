08df8eb939ec43f0cbcbd46fcea43c2dd3855b29
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-05-08 22:54:45 +0530

    working on step4

--- src/daohouse_frontend/src/Components/Dao/Step4.jsx
+++ src/daohouse_frontend/src/Components/Dao/Step4.jsx
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Step4.scss";
 import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 
 const Step4 = ({ setActiveStep }) => {
   const className = "DAO__Step4";
+  const [activeStage, setActiveStage] = useState(0);
 
   return (
     <React.Fragment>
       <div
         className={
           className +
-          "__form bg-[#F4F2EC] p-10 mx-4 rounded-lg flex flex-col gap-4"
+          "__form w-full bg-[#F4F2EC] p-10 mx-4 rounded-lg flex flex-col gap-4"
         }
       >
-        Step4
+        <ul className={className + "__steps flex flex-row gap-8"}>
+          <li
+            className={`list-disc text-lg font-semibold ${
+              activeStage == 0 ? "" : "opacity-50"
+            }`}
+          >
+            Proposal Creation
+          </li>
+          <li
+            className={`list-disc text-lg font-semibold ${
+              activeStage == 1 ? "" : "opacity-50"
+            }`}
+          >
+            Voting Permission
+          </li>
+        </ul>
+
+        <p className="font-semibold">Select Rights</p>
+        <p className="text-slate-700">
+          Decide what permissions you want to give to DAO groups for creating
+          things. You can adjust this later in settings.
+        </p>
+
+        <div className={className + "__table w-full overflow-x-auto"}>
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th style={{ textAlign: "left" }}>Actions</th>
+                <th>Council</th>
+                <th>All</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td className="text-nowrap">Change DAO Config</td>
+                <td className="text-center">
+                  <input type="checkbox" name="council" />
+                </td>
+                <td className="text-center">
+                  <input type="checkbox" name="group" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
 
       <div
