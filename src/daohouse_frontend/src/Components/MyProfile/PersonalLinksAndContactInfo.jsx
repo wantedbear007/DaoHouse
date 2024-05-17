8cddda8f5d5f660dbed0dce3504d082c1005abf9
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-05-17 17:15:39 +0530

    Profile - Route setting, Mypost done, Followers page working

--- src/daohouse_frontend/src/Components/MyProfile/PersonalLinksAndContactInfo.jsx
+++ src/daohouse_frontend/src/Components/MyProfile/PersonalLinksAndContactInfo.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PersonalLinksAndContactInfo = ({ links }) => {
+  const className = "PersonalLinksAndContactInfo";
+
   return (
-    <div className="bg-[#FFFFFF] md:text-[16px] text-[13px]  font-normal text-[#646464] p-2 my-2 rounded-lg">
+    <div className={className + " bg-[#FFFFFF] md:text-[16px] text-[13px]  font-normal text-[#646464] p-2 my-2 rounded-lg"}>
       <ul className="list-disc pl-4">
         {links.map((link, index) => (
           <li key={index} className="flex items-center my-4">
@@ -11,7 +13,8 @@ const PersonalLinksAndContactInfo = ({ links }) => {
               src={`../../../assets/${link.icon}.png`}
               alt={link.name}
               className="h-8 w-8 md:mr-12 mr-7"
-            />{" "}
+            />
+
             {link.value}
           </li>
         ))}
--- src/daohouse_frontend/src/Components/layouts/Footer.jsx
+++ src/daohouse_frontend/src/Components/layouts/Footer.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import twitter from "../../../assets/twitter.png"
-import telegram from "../../../assets/telegram.png"
-import linkedin from "../../../assets/linkedin.png"
-import discord from "../../../assets/discord.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/WhiteLogo.png";
+import twitter from "../../../assets/twitter.png";
+import telegram from "../../../assets/telegram.png";
+import linkedin from "../../../assets/linkedin.png";
+import discord from "../../../assets/discord.png";
 
 const Footer = () => {
-    return (
-        <>
-            <footer className="bg-[#05212C] py-12 text-center">
-                <div className="container mx-auto mb-8">
-                    <h2 className="text-xl font-bold text-white mb-4">LOGO</h2>
-                    <div className="flex justify-center mb-4">
-                        <Link to="/" className="text-[#728288] font-inter font-normal text-base mr-6 hover:underline">Home</Link>
-                        <Link to="/social-feed" className="text-[#728288]  font-inter font-normal text-base mr-6 hover:underline">Social Feed</Link>
-                        <Link to="/daos" className="text-[#728288]  font-inter font-normal text-base mr-6 hover:underline">DAOs</Link>
-                        <Link to="/proposals" className="text-[#728288]  font-inter font-normal text-base hover:underline">Proposals</Link>
-                    </div>
-                    <div className="flex justify-center mb-4">
-                        <img src={twitter} alt="Twitter" className="w-8 h-8 mr-4" />
-                        <img src={telegram} alt="Telegram" className="w-8 h-8 mr-4" />
-                        <img src={linkedin} alt="LinkedIn" className="w-8 h-8 mr-4" />
-                        <img src={discord} alt="Discord" className="w-8 h-8" />
-                    </div>
-                </div>
-
-            </footer>
-            <div className='bg-[#0E3746] py-4 flex justify-center items-center'>
-                <p className="text-white font-inter font-normal  text-[10px] md:text-base ">All rights reserved. © {new Date().getFullYear()} Your Company/Organization Name</p>
-
-            </div></>
-    );
+  return (
+    <React.Fragment>
+      <footer className="bg-[#05212C] py-12 text-center">
+        <div className="w-full mb-8">
+          <img src={logo} alt="DAO HOuse" className="w-32 mx-auto mb-10" />
+          <div className="flex justify-center mb-4">
+            <Link
+              to="/"
+              className="text-[#728288] font-inter font-normal text-base mr-6 hover:underline"
+            >
+              Home
+            </Link>
+            <Link
+              to="/social-feed"
+              className="text-[#728288]  font-inter font-normal text-base mr-6 hover:underline"
+            >
+              Social Feed
+            </Link>
+            <Link
+              to="/daos"
+              className="text-[#728288]  font-inter font-normal text-base mr-6 hover:underline"
+            >
+              DAOs
+            </Link>
+            <Link
+              to="/proposals"
+              className="text-[#728288]  font-inter font-normal text-base hover:underline"
+            >
+              Proposals
+            </Link>
+          </div>
+          <div className="flex justify-center mb-4">
+            <img src={twitter} alt="Twitter" className="w-8 h-8 mr-4" />
+            <img src={telegram} alt="Telegram" className="w-8 h-8 mr-4" />
+            <img src={linkedin} alt="LinkedIn" className="w-8 h-8 mr-4" />
+            <img src={discord} alt="Discord" className="w-8 h-8" />
+          </div>
+        </div>
+      </footer>
+      <div className="bg-[#0E3746] py-4 flex justify-center items-center">
+        <p className="text-white font-inter font-normal  text-[10px] md:text-base ">
+          All rights reserved. &copy; {new Date().getFullYear()} Your DAO House
+        </p>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Footer;
