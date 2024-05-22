81d4e18a8fb7c225d79cbdd0aaae82b1cad2c994
Anuj Tripathi <anuj.tripathi@quadbtech.com>
2024-05-22 19:23:12 +0530

    Merge remote-tracking branch 'origin/main' into anuj

---
+++ src/daohouse_frontend/src/Components/DaoProfile/Members.jsx
@@ -0,0 +1,216 @@
+import React, { useState } from "react";
+import "./Members.scss";
+import userImage from "../../../assets/commentUser.jpg";
+import { IoFilterSharp } from "react-icons/io5";
+import { LuSearch } from "react-icons/lu";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import { MdAddBox } from "react-icons/md";
+
+const Members = () => {
+  const [openedGroupIndex, setOpenedGroupIndex] = useState();
+  const className = "Member_and_policy";
+
+  return (
+    <div className={className + " mt-6"}>
+      <div className="flex items-center justify-between">
+        <h1 className="text-[24px] font-bold">Members and Policy</h1>
+        <button
+          onClick={() => {
+            Navigate("/create-proposal");
+          }}
+          className="flex justify-center items-center text-[16px] relative w-[220px] h-[50px] bg-white rounded-full hover:shadow-lg hover:bg-[#ECECEC] transition"
+        >
+          <span className="absolute text-[35px] font-thin left-5 bottom-[1px] ">
+            +
+          </span>
+          <span className="ml-6">Create Proposals</span>
+        </button>
+      </div>
+
+      <div className="bg-[#F4F2EC] rounded-[10px] mt-4">
+        <header className="flex flex-row items-center justify-between p-4 border-b border-slate-400">
+          <h1 className="font-semibold text-lg">Groups</h1>
+
+          <div className="flex flex-row items-center gap-x-2">
+            <div className="realtive flex flex-row items-center">
+              <input
+                type="search"
+                name="groups"
+                className="w-[400px] rounded-[2rem] py-2 pl-10 bg-[#F4F2EC] border border-[#AAC8D6]"
+                placeholder="Search by Propposal ID"
+              />
+              <LuSearch className="ml-4 absolute text-slate-400" />
+            </div>
+
+            <button className="flex flex-row items-center gap-2 px-4 py-2 bg-white rounded-[2rem]">
+              <IoFilterSharp />
+              Filter
+            </button>
+          </div>
+        </header>
+
+        <div
+          className={
+            className +
+            "__body p-4 bg-[#F4F2EC] rounded-lg gap-y-6 flex flex-col"
+          }
+        >
+          {groups.map((item, index) => (
+            <div
+              className={
+                className + "__body__group flex flex-col bg-white rounded-lg"
+              }
+              onClick={() => setOpenedGroupIndex(index)}
+            >
+              <header className="cursor-pointer flex flex-row items-center justify-between bg-[#AAC8D6] p-3 rounded-lg">
+                <p className="font-semibold">{item.groupName}</p>
+
+                <p className="font-semibold">
+                  {item.membersList.length} Members
+                </p>
+
+                {openedGroupIndex === index ? (
+                  <IoIosArrowDown className="font-bold" />
+                ) : (
+                  <IoIosArrowUp className="font-bold" />
+                )}
+              </header>
+
+              {openedGroupIndex === index && (
+                <div className={className + "__body__group__members p-8"}>
+                  {item.membersList.map((member) => (
+                    <div className="flex flex-col px-4 py-2 border border-[#97C3D3] rounded-lg">
+                      <div className="top flex flex-row items-start justify-between ">
+                        <section className="relative w-10 h-10">
+                          <img
+                            src={member.userImage}
+                            alt="Image"
+                            className="rounded-[50%] w-full h-full bottom-5 absolute"
+                          />
+                        </section>
+
+                        <MdAddBox className="mx-2 text-[#97C3D3] text-2xl" />
+                      </div>
+
+                      <div className="middle flex flex-row gap-x-4 item-center justify-between">
+                        <section className="details flex flex-col items-start">
+                          <p className="font-semibold text-lg">
+                            {member.userName}
+                          </p>
+                          <p className="text-sm">{member.userEmail}</p>
+                        </section>
+
+                        <section>
+                          <button className="bg-[#FFEDED] text-sm text-red-500 rounded-2xl shadow-lg px-4 py-2">
+                            Purpose to Remove
+                          </button>
+                        </section>
+                      </div>
+
+                      <div className="last flex flex-row align-start justify-evenly my-4">
+                        <section className="flex flex-col items-center px-2">
+                          <p className="text-sm">Total Votes</p>
+                          <h1 className="font-semibold text-2xl">
+                            {member.totalVotes}
+                          </h1>
+                        </section>
+
+                        <div className="h-7 w-0.5 bg-slate-400"></div>
+
+                        <section className="flex flex-col items-center">
+                          <p className="text-sm">In Favour</p>
+                          <h1 className="font-semibold text-2xl">
+                            {member.inFavour}
+                          </h1>
+                        </section>
+
+                        <div className="h-7 w-0.5 bg-slate-400"></div>
+
+                        <section className="flex flex-col items-center">
+                          <p className="text-sm">In Against</p>
+                          <h1 className="font-semibold text-2xl">
+                            {member.inAgainst}
+                          </h1>
+                        </section>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Members;
+
+const groups = [
+  {
+    groupName: "Council",
+    membersList: [
+      {
+        userKey: 1,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+      {
+        userKey: 2,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+      {
+        userKey: 3,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+      {
+        userKey: 4,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+    ],
+  },
+  {
+    groupName: "Group 1",
+    membersList: [
+      {
+        userKey: 1,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+      {
+        userKey: 2,
+        userImage: userImage,
+        userName: "Username.user",
+        userEmail: "gmail@gmail.com",
+        totalVotes: 25,
+        inFavour: 19,
+        inAgainst: 6,
+      },
+    ],
+  },
+];
--- src/daohouse_frontend/src/pages/DaoProfile/DaoProfile.jsx
+++ src/daohouse_frontend/src/pages/DaoProfile/DaoProfile.jsx
@@ -12,6 +12,7 @@ import SmallestCircle from "../../../assets/SmallestCircle.png";
 import MyProfileRectangle from "../../../assets/MyProfileRectangle.png";
 import ProposalsContent from "../../Components/DaoProfile/ProposalsContent";
 import FeedsContent from "../../Components/DaoProfile/FeedsContent";
+import Members from "../../Components/DaoProfile/Members";
 
 const DaoProfile = () => {
   const className = "DaoProfile";
@@ -44,6 +45,7 @@ const DaoProfile = () => {
       id: "lottie-smallCircle",
     },
   };
+
   // Animation options for the medium circle
   const defaultOptions3 = {
     loop: true,
@@ -174,11 +176,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("proposals");
             }}
-            className={`cursor-pointer ${
-              activeLink === "proposals"
+            className={`cursor-pointer ${activeLink === "proposals"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Proposals
           </a>
@@ -188,11 +189,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("feeds");
             }}
-            className={`cursor-pointer ${
-              activeLink === "feeds"
+            className={`cursor-pointer ${activeLink === "feeds"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Feeds
           </a>
@@ -202,11 +202,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("funds");
             }}
-            className={`cursor-pointer ${
-              activeLink === "funds"
+            className={`cursor-pointer ${activeLink === "funds"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Funds
           </a>
@@ -216,11 +215,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("member_policy");
             }}
-            className={`cursor-pointer ${
-              activeLink === "member_policy"
+            className={`cursor-pointer ${activeLink === "member_policy"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Member & Policy
           </a>
@@ -230,11 +228,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("followers");
             }}
-            className={`cursor-pointer ${
-              activeLink === "followers"
+            className={`cursor-pointer ${activeLink === "followers"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Followers
           </a>
@@ -244,11 +241,10 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("bounties");
             }}
-            className={`cursor-pointer ${
-              activeLink === "bounties"
+            className={`cursor-pointer ${activeLink === "bounties"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Bounties
           </a>
@@ -258,17 +254,17 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("settings");
             }}
-            className={`cursor-pointer ${
-              activeLink === "settings"
+            className={`cursor-pointer ${activeLink === "settings"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-            }`}
+              }`}
           >
             Settings
           </a>
         </div>
         {activeLink === "proposals" && <ProposalsContent />}
         {activeLink === "feeds" && <FeedsContent />}
+        {activeLink === "member_policy" && <Members />}
       </div>
     </div>
   );
