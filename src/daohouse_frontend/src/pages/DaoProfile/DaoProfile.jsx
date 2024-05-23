6bb572d8ce1399cda6425f8d95ff7ea327296022
Anuj Tripathi <anuj.tripathi@quadbtech.com>
2024-05-23 17:26:35 +0530

    Created the Followers subpage for the DAO Profile.

--- src/daohouse_frontend/src/pages/DaoProfile/DaoProfile.jsx
+++ src/daohouse_frontend/src/pages/DaoProfile/DaoProfile.jsx
@@ -13,6 +13,7 @@ import MyProfileRectangle from "../../../assets/MyProfileRectangle.png";
 import ProposalsContent from "../../Components/DaoProfile/ProposalsContent";
 import FeedsContent from "../../Components/DaoProfile/FeedsContent";
 import Members from "../../Components/DaoProfile/Members";
+import FollowersContent from "../../Components/DaoProfile/FollowersContent";
 
 const DaoProfile = () => {
   const className = "DaoProfile";
@@ -176,10 +177,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("proposals");
             }}
-            className={`cursor-pointer ${activeLink === "proposals"
+            className={`cursor-pointer ${
+              activeLink === "proposals"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Proposals
           </a>
@@ -189,10 +191,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("feeds");
             }}
-            className={`cursor-pointer ${activeLink === "feeds"
+            className={`cursor-pointer ${
+              activeLink === "feeds"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Feeds
           </a>
@@ -202,10 +205,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("funds");
             }}
-            className={`cursor-pointer ${activeLink === "funds"
+            className={`cursor-pointer ${
+              activeLink === "funds"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Funds
           </a>
@@ -215,10 +219,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("member_policy");
             }}
-            className={`cursor-pointer ${activeLink === "member_policy"
+            className={`cursor-pointer ${
+              activeLink === "member_policy"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Member & Policy
           </a>
@@ -228,10 +233,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("followers");
             }}
-            className={`cursor-pointer ${activeLink === "followers"
+            className={`cursor-pointer ${
+              activeLink === "followers"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Followers
           </a>
@@ -241,10 +247,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("bounties");
             }}
-            className={`cursor-pointer ${activeLink === "bounties"
+            className={`cursor-pointer ${
+              activeLink === "bounties"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Bounties
           </a>
@@ -254,10 +261,11 @@ const DaoProfile = () => {
               e.preventDefault();
               handleClick("settings");
             }}
-            className={`cursor-pointer ${activeLink === "settings"
+            className={`cursor-pointer ${
+              activeLink === "settings"
                 ? "underline text-[#0E3746]"
                 : "text-[#0E37464D]"
-              }`}
+            }`}
           >
             Settings
           </a>
@@ -265,6 +273,7 @@ const DaoProfile = () => {
         {activeLink === "proposals" && <ProposalsContent />}
         {activeLink === "feeds" && <FeedsContent />}
         {activeLink === "member_policy" && <Members />}
+        {activeLink === "followers" && <FollowersContent />}
       </div>
     </div>
   );
