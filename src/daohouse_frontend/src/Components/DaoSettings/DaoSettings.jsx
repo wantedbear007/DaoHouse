a06019889adf9de4398c958aabbd8ca9c3a2125f
Anuj Tripathi <anuj.tripathi@quadbtech.com>
2024-05-27 17:15:00 +0530

    Created the Settings subpages.

--- src/daohouse_frontend/src/Components/DaoSettings/DaoSettings.jsx
+++ src/daohouse_frontend/src/Components/DaoSettings/DaoSettings.jsx
@@ -1,10 +1,15 @@
 import React, { useRef, useState } from "react";
-import './DaoSettings.scss'
+import "./DaoSettings.scss";
 import { FaCircleCheck } from "react-icons/fa6";
 import Step1 from "./Step1";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
+import Step4 from "./Step4";
+import Step5 from "./Step5";
+import Step6 from "./Step6";
 
 const DaoSettings = () => {
-  const className = "DaoSettings"
+  const className = "DaoSettings";
   const [activeStep, setActiveStep] = React.useState(0);
 
   const [data, setData] = useState({
@@ -24,29 +29,28 @@ const DaoSettings = () => {
   const Form = () => {
     switch (activeStep) {
       case 0:
-      return (
-        <Step1 setData={setData} setActiveStep={setActiveStep} />
-      );
+        return <Step1 setData={setData} setActiveStep={setActiveStep} />;
       case 1:
-      // return (
-      //   <Step2 setData={setData} setActiveStep={setActiveStep} />
-      // );
+        return <Step2 setData={setData} setActiveStep={setActiveStep} />;
       case 2:
-      // return (
-      //   <Step3 setData={setData} setActiveStep={setActiveStep} Step1Ref={Step1Ref} Step4Ref={Step4Ref} />
-      // );
+        return (
+          <Step3
+            setData={setData}
+            setActiveStep={setActiveStep}
+            Step1Ref={Step1Ref}
+            Step4Ref={Step4Ref}
+          />
+        );
       case 3:
-      // return (
-      //   <Step4 data={data} setData={setData} setActiveStep={setActiveStep} />
-      // );
+        return (
+          <Step4 data={data} setData={setData} setActiveStep={setActiveStep} />
+        );
       case 4:
-      // return (
-      //   <Step5 setData={setData} setActiveStep={setActiveStep} />
-      // );
+        return <Step5 setData={setData} setActiveStep={setActiveStep} />;
       case 5:
-      // return (
-      //   <Step6 data={data} setData={setData} setActiveStep={setActiveStep} />
-      // );
+        return (
+          <Step6 data={data} setData={setData} setActiveStep={setActiveStep} />
+        );
       default:
         return null;
     }
@@ -54,7 +58,6 @@ const DaoSettings = () => {
 
   return (
     <React.Fragment>
-
       {/** Heading */}
       <div className={className + " mt-6 bg-[#c8ced3]"}>
         <h1 className="mobile:text-2xl text-lg font-bold py-1">
@@ -92,7 +95,9 @@ const DaoSettings = () => {
               ) : (
                 <FaCircleCheck className="mobile:text-2xl text-[1.2rem] text-[#0E3746]" />
               )}
-              <span className="text-nowrap mobile:text-base text-xs">{name}</span>
+              <span className="text-nowrap mobile:text-base text-xs">
+                {name}
+              </span>
             </div>
           ))}
         </div>
@@ -105,7 +110,6 @@ const DaoSettings = () => {
 };
 
 export default DaoSettings;
-
 
 const steps = [
   {
