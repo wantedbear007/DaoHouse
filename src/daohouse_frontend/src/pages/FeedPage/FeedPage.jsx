83e13c76c9faa37831b47d93db8c51f66d4abd01
NehaKumari0102 <neha.kumari@quadbtech.com>
2024-05-30 12:41:27 +0530

    Social feed page responsivness done

--- src/daohouse_frontend/src/pages/FeedPage/FeedPage.jsx
+++ src/daohouse_frontend/src/pages/FeedPage/FeedPage.jsx
@@ -26,7 +26,7 @@ const FeedPage = () => {
       <div
         className={
           className +
-          "__filter w-100 h-[25vh] p-20 flex flex-col items-start justify-center"
+          "__filter w-100 mobile:h-[25vh] h-[17vh] big_phone:p-20 small_phone:p-10 p-4 flex flex-col items-start justify-center"
         }
         style={{
           backgroundImage: `url("${image}")`,
@@ -35,20 +35,20 @@ const FeedPage = () => {
           backgroundPosition: "center",
         }}
       >
-        <h1 className="text-3xl p-3 text-white">
+        <h1 className="mobile:text-5xl text-3xl p-3 text-white">
           Social Feed
         </h1>
 
-        <div className={className + "__buttons flex flex-row  border-t-2 border-white"}>
+        <div className={className + "__buttons flex flex-row border-t-2 border-white"}>
           <button
-            className={`px-6 py-2 text-lg text-white ${!active.all ? "" : "shadow-lg font-semibold"
+            className={`px-6 py-2 mobile:text-lg text-sm text-white ${!active.all ? "" : "shadow-lg font-semibold"
               }`}
             onClick={setAllActive}
           >
             All
           </button>
           <button
-            className={`px-6 py-2 text-lg text-white ${!active.latest ? "" : "shadow-lg font-semibold"
+            className={`px-6 py-2 mobile:text-lg text-sm text-white ${!active.latest ? "" : "shadow-lg font-semibold"
               }`}
             onClick={setLatestActive}
           >
@@ -60,18 +60,18 @@ const FeedPage = () => {
       <div
         className={
           className +
-          "__label bg-[#c8ced3] py-8 px-10 flex flex-row w-full justify-between items-center"
+          "__label bg-[#c8ced3] small_phone:py-8 py-5 mobile:px-10 px-5 flex flex-row w-full justify-between items-center"
         }
       >
-        <p className="text-4xl px-8 flex flex-row items-center gap-4">
+        <p className="small_phone:text-4xl text-3xl big_phone:px-8 flex flex-row items-center gap-4">
           {active.all ? "All" : "Latest"}
           <div className="flex flex-col items-start">
-            <div className="w-32 border-t-2 border-black"></div>
-            <div className="w-14 mt-2 border-t-2 border-black"></div>
+            <div className="mobile:w-32 w-12 border-t-2 border-black"></div>
+            <div className="mobile:w-14 w-8 small_phone:mt-2 mt-1 border-t-2 border-black"></div>
           </div>
         </p>
 
-        <button className="bg-white gap-2 px-4 shadow-xl py-2 px-4 rounded-full shadow-md flex items-center space-x-4 rounded-2xl hover:bg-[#ececec] hover:scale-105 transition">
+        <button className="bg-white small_phone:gap-2 gap-1 mobile:px-5 p-2 small_phone:text-base text-sm shadow-xl rounded-full shadow-md flex items-center rounded-2xl hover:bg-[#ececec] hover:scale-105 transition">
           <HiPlus />
           Create Post
         </button>
@@ -79,7 +79,7 @@ const FeedPage = () => {
 
       <div
         className={
-          className + "__postCards px-10 pb-10 bg-[#c8ced3] gap-8 flex flex-col"
+          className + "__postCards mobile:px-10 px-6 pb-10 bg-[#c8ced3] gap-8 flex flex-col"
         }
       >
         {feed && feed.map((post, i) => <PostCard post={post} key={i} />)}
