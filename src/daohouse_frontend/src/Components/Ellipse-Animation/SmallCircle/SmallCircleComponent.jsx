cae92eb51dd44099eb11215f7049115b559fe768
Anuj Tripathi <anuj.tripathi@quadbtech.com>
2024-06-03 18:18:22 +0530

    Made the necessary changes as required for the create post popup and my profile page

--- src/daohouse_frontend/src/Components/Ellipse-Animation/SmallCircle/SmallCircleComponent.jsx
+++ src/daohouse_frontend/src/Components/Ellipse-Animation/SmallCircle/SmallCircleComponent.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 const SmallCircleComponent = ({ imgSrc }) => {
-  return (
-    <img
-      src={imgSrc}
-      alt="Small Circle"
-      className="absolute md:w-[43px] md:h-[43px] w-[26px] h-[26px] md:right-[25rem] right-[8.5rem] -translate-y-full md:top-[30%] top-[24%]"
-    />
-  );
+  return <img src={imgSrc} alt="Small Circle" className="w-full" />;
 };
 
 export default SmallCircleComponent;
