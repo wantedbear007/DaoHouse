ff551eac60c5508eb0c40e4afcff1faa8bdad70d
ShubhamQuadb <shubham.verma@quadbtech.com>
2024-08-21 15:20:48 +0530

    fixed suporting partner aligment

--- src/daohouse_frontend/src/Components/Home/SupportingPartners.jsx
+++ src/daohouse_frontend/src/Components/Home/SupportingPartners.jsx
@@ -1,40 +1,90 @@
 import React from "react";
-import bmw from "../../../assets/bmw.png";
-import starbucks from "../../../assets/starbucks.png";
-import panda from "../../../assets/panda.png";
-import image5 from "../../../assets/image5.png";
-import pandg from "../../../assets/pandg.png";
-import Widget from "./Widget";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/WhiteLogo.png";
+import tw from "../../../assets/tw.png";
+import telegram from "../../../assets/telegram.png";
+import linkedin from "../../../assets/linkedin.png";
+import discord from "../../../assets/discord.png";
 
-const SupportingPartners = () => {
+const Footer = () => {
   return (
-    <div className="bg-gray-200 mobile:py-[10rem] py-20">
-      <div className="justify-center mt-[-200px] items-center flex">
-        <Widget />
+    <React.Fragment>
+      <footer className="bg-[#05212C] small_phone:py-12 py-8 text-center">
+        <div className="w-full small_phone:mb-8 mb-4">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="DAO House"
+              className="small_phone:w-32 w-20 mx-auto small_phone:mb-10 mb-6"
+            />
+          </Link>
+
+          {/* Links Section */}
+          <div className="flex justify-center mb-4 gap-x-3">
+            <Link
+              to="/"
+              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
+            >
+              Home
+            </Link>
+            <Link
+              to="/social-feed"
+              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
+            >
+              Social Feed
+            </Link>
+            <Link
+              to="/dao"
+              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
+            >
+              DAOs
+            </Link>
+            <Link
+              to="/proposals"
+              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
+            >
+              Proposals
+            </Link>
+          </div>
+
+          {/* Social Media Icons Section with Flex Layout */}
+          <div className="container flex justify-between items-center small_phone:px-20 px-8">
+            <div className="flex gap-4">
+              <img
+                src={tw}
+                alt="Twitter"
+                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
+              />
+              <img
+                src={telegram}
+                alt="Telegram"
+                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
+              />
+            </div>
+            <div className="flex gap-4">
+              <img
+                src={linkedin}
+                alt="LinkedIn"
+                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
+              />
+              <img
+                src={discord}
+                alt="Discord"
+                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+
+      {/* Footer Bottom Section */}
+      <div className="bg-[#0E3746] py-4 flex justify-center items-center">
+        <p className="text-white font-inter font-normal text-[10px] md:text-base">
+          &copy; {new Date().getFullYear()}, DAO House. All rights reserved.
+        </p>
       </div>
-      <div className="container mx-auto text-center pt-4 mb-8 mt-8">
-        <h2 className="text-2xl mobile:text-4xl font-medium mobile:mb-12 mb-4">
-          Supporting Partners
-        </h2>
-      </div>
-      <div className="container mobile:gap-5 gap-2 px-20 flex md:flex-wrap justify-center">
-        {renderPartnerImages()}
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
-const renderPartnerImages = () => {
-  const partnerImages = [bmw, starbucks, panda, image5, pandg];
-
-  return partnerImages.map((image, index) => (
-    <img
-      key={index}
-      src={image}
-      alt={`Partner ${index + 1}`}
-      className="tablet:w-28 mobile:w-16 w-10 tablet:h-28 mobile:w-16 w-10 h-auto rounded-full "
-    />
-  ));
-};
-
-export default SupportingPartners;
+export default Footer;
