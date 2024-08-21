52800feda41c12a1d22cdbb32858634426479d2c
ShubhamQuadb <shubham.verma@quadbtech.com>
2024-08-21 15:26:03 +0530

    fix suporting partner alignment

--- src/daohouse_frontend/src/Components/Home/SupportingPartners.jsx
+++ src/daohouse_frontend/src/Components/Home/SupportingPartners.jsx
@@ -1,90 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../../../assets/WhiteLogo.png";
-import tw from "../../../assets/tw.png";
-import telegram from "../../../assets/telegram.png";
-import linkedin from "../../../assets/linkedin.png";
-import discord from "../../../assets/discord.png";
+import bmw from "../../../assets/bmw.png";
+import starbucks from "../../../assets/starbucks.png";
+import panda from "../../../assets/panda.png";
+import image5 from "../../../assets/image5.png";
+import pandg from "../../../assets/pandg.png";
+import Widget from "./Widget";
 
-const Footer = () => {
+const SupportingPartners = () => {
   return (
-    <React.Fragment>
-      <footer className="bg-[#05212C] small_phone:py-12 py-8 text-center">
-        <div className="w-full small_phone:mb-8 mb-4">
-          <Link to="/">
-            <img
-              src={logo}
-              alt="DAO House"
-              className="small_phone:w-32 w-20 mx-auto small_phone:mb-10 mb-6"
-            />
-          </Link>
-
-          {/* Links Section */}
-          <div className="flex justify-center mb-4 gap-x-3">
-            <Link
-              to="/"
-              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
-            >
-              Home
-            </Link>
-            <Link
-              to="/social-feed"
-              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
-            >
-              Social Feed
-            </Link>
-            <Link
-              to="/dao"
-              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
-            >
-              DAOs
-            </Link>
-            <Link
-              to="/proposals"
-              className="text-[#728288] small_phone:text-base text-sm font-inter font-normal hover:underline"
-            >
-              Proposals
-            </Link>
-          </div>
-
-          {/* Social Media Icons Section with Flex Layout */}
-          <div className="container flex justify-between items-center small_phone:px-20 px-8">
-            <div className="flex gap-4">
-              <img
-                src={tw}
-                alt="Twitter"
-                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
-              />
-              <img
-                src={telegram}
-                alt="Telegram"
-                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
-              />
-            </div>
-            <div className="flex gap-4">
-              <img
-                src={linkedin}
-                alt="LinkedIn"
-                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
-              />
-              <img
-                src={discord}
-                alt="Discord"
-                className="small_phone:w-8 w-6 small_phone:h-8 h-6 object-contain"
-              />
-            </div>
-          </div>
-        </div>
-      </footer>
-
-      {/* Footer Bottom Section */}
-      <div className="bg-[#0E3746] py-4 flex justify-center items-center">
-        <p className="text-white font-inter font-normal text-[10px] md:text-base">
-          &copy; {new Date().getFullYear()}, DAO House. All rights reserved.
-        </p>
+    <div className="bg-gray-200 mobile:py-[10rem] py-20">
+      <div className="justify-center mt-[-200px] items-center flex">
+        <Widget />
       </div>
-    </React.Fragment>
+      <div className="container mx-auto text-center pt-4 mb-8 mt-8">
+        <h2 className="text-2xl mobile:text-4xl font-medium mobile:mb-12 mb-4">
+          Supporting Partners
+        </h2>
+      </div>
+      <div className="container flex justify-between items-center mobile:px-20 px-8">
+        <div className="flex gap-6">
+          <img
+            src={bmw}
+            alt="BMW"
+            className="tablet:w-36 mobile:w-28 w-20 h-auto rounded-full"
+          />
+        </div>
+        <div className="flex gap-4">
+        <img
+            src={starbucks}
+            alt="Starbucks"
+            className="tablet:w-36 mobile:w-28 w-20 h-auto rounded-full"
+          />
+        </div>
+        <img
+          src={panda}
+          alt="Panda"
+          className="tablet:w-40 mobile:w-32 w-24 h-auto rounded-full"
+        />
+        <div className="flex gap-6">
+          <img
+            src={image5}
+            alt="Image5"
+            className="tablet:w-36 mobile:w-28 w-20 h-auto rounded-full"
+          />
+          </div>
+          <div className="4">
+          <img
+            src={pandg}
+            alt="P&G"
+            className="tablet:w-36 mobile:w-28 w-20 h-auto rounded-full"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default Footer;
+export default SupportingPartners;
