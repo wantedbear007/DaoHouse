6d7895dd8e86b62180409af1fa19108c7b86d12a
prabhoffical <prabhjot.kaur@quadbtech.com>
2024-08-23 06:13:30 +0000

    change in deployment

--- deploy_local.sh
+++ deploy_local.sh
@@ -31,7 +31,8 @@ MINTER=$(dfx --identity default identity get-principal)
 DEFAULT=$(dfx --identity default identity get-principal)
 RECIEVER=$(dfx --identity reciever identity get-principal) # m2zqz-pr5r2-ozayk-w5trf-mt6mw-7vuys-mitrw-4qdpb-dm5p7-77ey6-fae
 PRO=$(dfx --identity minter identity get-principal) # rmehg-adw5r-6trpq-epk4r-tyl4c-dd2u4-erbw4-kcjzr-rrjpf-dfvi2-oae
-BHANU=$(dfx --identity Bhanu identity get-principal)  # yxtej-lmfuu-rp3yv-xzu2h-6q43c-7iast-yiwff-z552q-6ugas-pyd6b-fae
+# BHANU=$(dfx --identity Bhanu identity get-principal) 
+ # yxtej-lmfuu-rp3yv-xzu2h-6q43c-7iast-yiwff-z552q-6ugas-pyd6b-fae
 
 TOKEN_SYMBOL=TOK
 TOKEN_NAME="DAOTOKEN"
@@ -67,7 +68,7 @@ record {
     transfer_fee = ${TRANSFER_FEE};
     metadata = vec {}; 
     initial_balances = vec {
-        record { record { owner = principal \"${BHANU}\" }; ${PRE_MINTED_TOKENS} };
+        record { record { owner = principal \"${PRO}\" }; ${PRE_MINTED_TOKENS} };
         record { record { owner = principal \"${PRO}\" }; ${PRE_MINTED_TOKENS} };
         record { record { owner = principal \"${RECIEVER}\" }; ${DIFFERENT} }
     };
