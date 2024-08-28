b7f7a22d09f54b66c37cfa2a0b47697e588319f4
prabhoffical <prabhjot.kaur@quadbtech.com>
2024-08-28 06:34:54 +0000

    nodata

--- src/daohouse_frontend/src/Components/Dao/NoDataComponent.jsx
+++ src/daohouse_frontend/src/Components/Dao/NoDataComponent.jsx
@@ -6,14 +6,14 @@ const NoDataComponent = ({ setJoinedDAO  }) => {
     <div className="w-full flex flex-col items-center justify-center p-20">
       <img src={nodata} alt="No Data" className="mb-4" />
       <p className="text-center text-gray-700 mb-4 text-[16px]">
-        You have not joined any DAO
+        You have not created any DAO
       </p>
-      <button
+      {/* <button
         onClick={() => setJoinedDAO(true)}
         className="flex-1 bg-[#0E3746] border-2 border-[#0E3746] text-white px-8 py-2 rounded-[3rem]"
       >
         Join DAO
-      </button>
+      </button> */}
     </div>
   );
 };
