47598a84741d174d41fcfe8fce54d6810485dfb2
ShubhamQuadb <shubham.verma@quadbtech.com>
2024-09-07 16:50:42 +0530

    fix  input  alignment

--- src/daohouse_frontend/src/pages/EditProfile/EditPersonalLinksAndContactInfo.jsx
+++ src/daohouse_frontend/src/pages/EditProfile/EditPersonalLinksAndContactInfo.jsx
@@ -8,69 +8,70 @@ const EditPersonalLinksAndContactInfo = ({
 }) => {
   return (
     <div className="bg-white p-4 rounded-lg shadow-md">
-      <div className="mb-4 flex flex-col">
-
-        {/* Contact Number */}
-        <div className="flex flex-col mb-4">
-          <div className="flex items-center">
-            <label
-              htmlFor="contactNo"
-              className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
-            >
-              Contact No.<span className="text-red-500">*</span>
-            </label>
-            <div className="flex-grow ml-2">
-              <input
-                id="contact_number"
-                name="contact_number"
-                type="tel"
-                value={profileData.contact_number}
-                onChange={handleInputChange}
-                placeholder="0123456789"
-                className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
-              />
-            </div>
-          </div>
-          {errors.contact_number && (
-            <p className="text-red-500 text-xs mt-1">{errors.contact_number}</p>
-          )}
+  <div className="mb-4 flex flex-col">
+    
+    <div className="flex flex-col mb-4">
+      <div className="flex items-center">
+        <label
+          htmlFor="contactNo"
+          className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
+        >
+          Contact No.<span className="text-red-500">*</span>
+        </label>
+        <div className="flex flex-grow translate-x-[8px]">
+          <input
+            id="contact_number"
+            name="contact_number"
+            type="tel"
+            value={profileData.contact_number}
+            onChange={handleInputChange}
+            placeholder="0123456789"
+            className="py-2 px-3 w-full lg:w-[50%]    md:w-[100%] lg:ml-16 ml-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
+          />
         </div>
+      </div>
+      {errors.contact_number && (
+        <p className="text-red-500 text-xs mt-1">{errors.contact_number}</p>
+      )}
+    </div>
 
-        {/* Email */}
-        <div className="flex flex-col mb-4">
-          <div className="flex items-center">
-            <label
-              htmlFor="email"
-              className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
-            >
-              Email <span className="text-red-500">*</span>
-            </label>
-            <div className="flex-grow ml-2">
-              <input
-                id="email"
-                name="email_id"
-                type="email"
-                value={profileData.email_id}
-                onChange={handleInputChange}
-                placeholder="Emailid.id@example.com"
-                className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
-              />
-            </div>
-          </div>
-          {errors.email_id && (
-            <p className="text-red-500 text-xs mt-1">{errors.email_id}</p>
-          )}
+    {/* Email */}
+    <div className="flex flex-col mb-4">
+      <div className="flex items-center">
+        <label
+          htmlFor="email"
+          className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
+        >
+          Email <span className="text-red-500">*</span>
+        </label>
+        <div className="flex-grow ml-2">
+          <input
+            id="email"
+            name="email_id"
+            type="email"
+            value={profileData.email_id}
+            onChange={handleInputChange}
+            placeholder="Emailid.id@example.com"
+            className="py-2 px-3 w-full lg:w-[50%]  md:w-[100%] lg:ml-16 ml-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
+          />
         </div>
+      </div>
+      {errors.email_id && (
+        <p className="text-red-500 text-xs mt-1">{errors.email_id}</p>
+      )}
+    </div>
+  
+
 
         {/* Twitter */}
         <div className="flex items-center mb-4">
           <label
             htmlFor="twitter"
-            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
+            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] mr-2 md:w-[100px] w-[70px]"
           >
             Twitter
           </label>
-          <div className="flex-grow ml-2">
+          <div className="flex flex-grow">
             <input
               id="twitter"
               name="twitter_id"
@@ -78,7 +79,7 @@ const EditPersonalLinksAndContactInfo = ({
               value={profileData.twitter_id}
               onChange={handleInputChange}
               placeholder="http://www.example.com"
-              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
+              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] lg:ml-16 ml-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm box-border text-[12px]"
             />
           </div>
         </div>
@@ -87,11 +88,11 @@ const EditPersonalLinksAndContactInfo = ({
         <div className="flex items-center mb-4">
           <label
             htmlFor="telegram"
-            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
+            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] mr-2 md:w-[100px] w-[70px]"
           >
             Telegram
           </label>
-          <div className="flex-grow ml-2">
+          <div className="flex flex-grow">
             <input
               id="telegram"
               name="telegram"
@@ -99,7 +100,7 @@ const EditPersonalLinksAndContactInfo = ({
               value={profileData.telegram}
               onChange={handleInputChange}
               placeholder="http://www.example.com"
-              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
+              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] lg:ml-16 ml-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm box-border text-[12px]"
             />
           </div>
         </div>
@@ -108,11 +109,11 @@ const EditPersonalLinksAndContactInfo = ({
         <div className="flex items-center mb-4">
           <label
             htmlFor="website"
-            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] md:w-[100px] w-[70px]"
+            className="block md:text-[16px] text-[12px] font-semibold text-[#05212C] mr-2 md:w-[100px] w-[70px]"
           >
             Website
           </label>
-          <div className="flex-grow ml-2">
+          <div className="flex flex-grow">
             <input
               id="website"
               name="website"
@@ -120,7 +121,7 @@ const EditPersonalLinksAndContactInfo = ({
               value={profileData.website}
               onChange={handleInputChange}
               placeholder="http://www.example.com"
-              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm text-[12px]"
+              className="py-2 px-3 w-full lg:w-[50%] md:w-[100%] lg:ml-16 ml-1 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#05212C] focus:border-[#05212C] sm:text-sm box-border text-[12px]"
             />
           </div>
         </div>
@@ -131,7 +132,7 @@ const EditPersonalLinksAndContactInfo = ({
         </button>
         <button
           onClick={handleSaveChangesClick}
-          className="py-2 w-[126px] border border-[#0E3746] bg-[#0E3746] text-white hover:bg-[#0E37464D] hover:border-[#0E37464D] rounded-[27px] transition duration-200 ease-in-out"
+          className="py-2 w-[126px] border border-[#0E3746] bg-[#0E3746] text-white  hover:bg-[#0E37464D] hover:border-[#0E37464D] rounded-[27px] transition duration-200 ease-in-out"
         >
           Save Changes
         </button>
